5b2b4f951befa1f1bbd56f688f2749a0a8f94a5f
Bianca Miranda <miranda.bianca41@gmail.com>
2023-07-17 01:34:13 -0300

    Mecanica de Coleta de Chaves e Audio de Coleta

---
+++ Assets/Scripts/KeysController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeysController : MonoBehaviour
+{
+    private bool canCollect;
+    public Transform player;
+    public AudioClip collectKey;
+    private float distanceToPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+        if (distanceToPlayer <= 3f)
+        {
+            canCollect = true;
+        }
+        else if (distanceToPlayer > 3f)
+        {
+            canCollect = false;
+        }
+        if(Input.GetKey(KeyCode.K) && canCollect)
+        {
+            GetComponent<AudioSource>().PlayOneShot(collectKey);
+            Invoke("DestroyKey", 1f);
+        }
+
+    }
+    void DestroyKey()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(transform.position, 3f);
+    }
+}
+
