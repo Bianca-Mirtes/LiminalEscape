a50ec4dc6d64f9dc9d085e45ba0b3d4cfcc765d2
Bianca-Mirtes <miranda.bianca41@gmail.com>
2023-07-18 01:19:08 -0300

    Spawner de Armas e Mecanica Agachar consertada

--- Assets/Scripts/SpawnerToolsController.cs
+++ Assets/Scripts/SpawnerToolsController.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SpawnerToolsController : MonoBehaviour
 {
+    public List<GameObject> spawnPoints;
+    public GameObject[] tools;
+    public GameObject toolsSlot;
+
+    private int qntTools = 4;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        for(int ii=0; ii < qntTools; ii++)
+        {
+            int posRandom = Random.Range(0, spawnPoints.Count);
+            Instantiate(tools[Random.Range(0, tools.Length)], spawnPoints[posRandom].transform.position, Quaternion.identity, toolsSlot.transform);
+            spawnPoints.Remove(spawnPoints[posRandom]);
+        }
     }
 }
